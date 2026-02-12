f111ba40bd1d536f0637f62adf99c6a4b91f2ce7
trm-47 <pdiperjuangan.diy@gmail.com>
2026-02-12 20:20:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ async function submitSeluruhData() {
     if(!data) return alert("Data kosong!");
     const btn = document.querySelector('.btn-final');
     btn.disabled = true; btn.innerHTML = "⏳ MENGIRIM...";
-    const URL_API = 'https://script.google.com/macros/s/AKfycbyFh3y-hh_mV7pn9jAWMoX1ISi8640zQoaQPNddydnI5uRojIzNxgAJYeLRY5ekDTCMQw/exec';
+    const URL_API = 'https://script.google.com/macros/s/AKfycbyE4ebCnjZ3aUJQ9gR0Qe_mgBM5GK18xHr74SIzbhsxvxM_-RkKOE6CNxaXHuAZwMQ1YQ/exec';
     try {
         await fetch(URL_API, { method: 'POST', mode: 'no-cors', body: JSON.stringify(data) });
         alert("MERDEKA! Data Terkirim."); localStorage.clear(); window.location.href = 'finish.html';
